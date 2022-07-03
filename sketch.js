10d0fd7f9fabef076f879afca8397ca72f3f5083
Darlan de Sousa <88466141+darlandesousa15@users.noreply.github.com>
2022-07-03 20:07:50 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload(){
   carro5 = loadImage("imagens/carro-3.png");
   carro6 = loadImage("imagens/carro-1.png");
   carros = [carro1, carro2, carro3, carro4, carro5, carro6];
-
+//SOM
   somDaTrilha = loadSound("sons/trilha.mp3");
   somColisao = loadSound("sons/colidiu.mp3");
   somDoPonto = loadSound("sons/pontos.wav");
